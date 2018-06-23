99f5b21467157c6ab1789cc48f35a1c8a58a75ca
DESKTOP-DE5BIT9\hxx14 <xhrsteven@gmail.com>
2018-06-22 21:40:01 -0500

    no message

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
             method:'PUT',
             url:'/burgers/'+burger_id
         }).then(function(data){
-            location.reload();
+            location.reload(); 
         });
     }));
 });
